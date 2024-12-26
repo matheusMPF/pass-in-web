81709538a8e2c975963552ec9cf58714896b7a56
Matheus Magalhães <matheus.mpf18@gmail.com>
2024-12-26 20:26:14 -0300

    Fazendo build

--- src/data/attendees.ts
+++ src/data/attendees.ts
@@ -1,13 +1,12 @@
-import { faker } from '@faker-js/faker'
+import { faker } from "@faker-js/faker";
 
 //Criando dados ficticios a partir desta dependência "npm install @faker-js/faker --save-dev"
 export const attendees = Array.from({ length: 222 }).map(() => {
-    return{
-        id: faker.number.int({min: 10000, max:20000}),
-        name: faker.person.fullName(),
-        email: faker.internet.email().toLocaleLowerCase(),
-        createdAt: faker.date.recent({ days: 30 }),
-        checkedInAt: faker.date.recent({ days: 7 }),
-
-    }
-})
+  return {
+    id: faker.number.int({ min: 10000, max: 20000 }),
+    name: faker.person.fullName(),
+    email: faker.internet.email().toLocaleLowerCase(),
+    createdAt: faker.date.recent({ days: 30 }),
+    checkedInAt: faker.date.recent({ days: 7 }),
+  };
+});
